c36e5a74fdbf1bd49b783c7c307a4f0d452a8149
Ronda Wylie <rondawylie@yahoo.com>
2018-01-09 15:02:28 -0600

    button works

--- weather-api/main.js
+++ weather-api/main.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-$('#loading-image').show();
+      $('#loading-image').show();
       $.ajax({
         url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + "&units=imperial" + '&APPID=19e7d5a5cc69d0dcb0892a99baf2a999',
         type: "GET",
@@ -14,18 +14,24 @@ $('#loading-image').show();
           $("#icon").html(data.weather[0].description + '<img style="width: 50px;" src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">');
           $("#your-location").html("Your Location:");
           $("#current_location").html(data.name);
-          $("#temperature").html("Current Temperature")
+          $("#temperature").html("Current Temperature");
           $("#temp").html(data.main.temp + " F");
-          $("#buttons").html("<button type='button'>Fahrenheit</button>" + " <button type='button'>Celsius</button>");
+          // $("#tempc").html(data.main.);
+          $("#buttons").html("<button type='button'>Fahrenheit</button>" + " <button id='cel' onclick='changeToCelsius();' type='button'>Celsius</button>");
         },
         complete: function(){
         $('#loading-image').hide();
-
       }
       });
     });
   }
-});
+  // function changeToCelsius() {
+  //   alert("hey")
+  // }
+     });
+function changeToCelsius() {
+  alert("hey")
+}
 // $(document).ready(function() {
 //   $('#submitWeather').click(function() {
 //     var city = $(#city).val();
