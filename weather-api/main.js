a4e4db6ec2fd23b10fbc7caf339b1599090e23f5
Ronda Wylie <rondawylie@yahoo.com>
2018-01-13 14:16:12 -0600

    wiki-app setup

--- weather-api/main.js
+++ weather-api/main.js
@@ -24,35 +24,32 @@ $(document).ready(function() {
       }
       });
     });
+
+    $('#cel').click(function(){
+       $.ajax({
+           url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + "&units=metric" + '&APPID=19e7d5a5cc69d0dcb0892a99baf2a999',
+           // name of the callback parameter
+           jsonp: "callback",
+           // tell jQuery we're expecting JSONP
+           dataType: "jsonp",
+           //what we want
+           data: {
+               units:"metric",
+               mode: "json"
+           },
+           // work with the response
+           success: function(response) {
+                 var tempC = response.list[0].main.temp;
+              $('#tempc').html('<span>'+response.list[0].main.temp+'</span><span class="t">* C</span>');
+           }
+       });
+      });
+
+
+
   }
-  // function changeToCelsius() {
-  //   alert("hey")
-  // }
      });
 function changeToCelsius() {
   alert("hey")
+
 }
-// $(document).ready(function() {
-//   $('#submitWeather').click(function() {
-//     var city = $(#city).val();
-//
-//     if(city != '') {
-//       $.ajax({
-//         url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=19e7d5a5cc69d0dcb0892a99baf2a999
-// ",
-// type= "GET",
-// dataType: "jsonp",
-// success: function(data){
-//   console.log(data);
-// }
-//
-//       });
-//     } else {
-//       $("#error").html('Field cannot be empty');
-//     }
-//
-//
-//   });
-//
-//
-// });
