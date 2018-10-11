32f73737a9a3814e4035682b340db82554716724
Ronda Wylie <rondawylie@yahoo.com>
2018-10-10 22:10:22 -0500

    Object Literal

--- practice/javascript_algorithms/main.js
+++ practice/javascript_algorithms/main.js
@@ -2,3 +2,17 @@
 
 //template literals:
 //const resultDisplayArray = arr.map(item => `<li class="text-warning">${item}</li>`);
+
+//example of Object Literal Declarations using simple fields
+
+// const createPerson = (name, age, gender) => {
+//   "use strict";
+  // change code below this line
+  // return {
+  //   name,
+  //   age,
+  //   gender
+  // };
+  // change code above this line
+// };
+// console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
