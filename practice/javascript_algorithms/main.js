767bb817847663c5d107fd3735553cde1b0fa53f
Ronda Wylie <rondawylie@yahoo.com>
2018-10-13 18:26:48 -0500

    ternary operator

--- practice/javascript_algorithms/main.js
+++ practice/javascript_algorithms/main.js
@@ -87,3 +87,9 @@
 // }
 //
 // convertToInteger("10011");
+//ternary operator used for if statements
+// function checkEqual(a, b) {
+//   return (a = b ? true : false );
+// }
+//
+// checkEqual(1, 2);
